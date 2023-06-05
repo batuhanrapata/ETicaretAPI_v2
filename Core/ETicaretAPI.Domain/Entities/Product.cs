5805bb1b689ebfa8cb1685f35a993dfd80d603f2
Batuhan Rapata <56839590+batuhanrapata@users.noreply.github.com>
2023-06-05 14:01:05 +0300

    commit

--- Core/ETicaretAPI.Domain/Entities/Product.cs
+++ Core/ETicaretAPI.Domain/Entities/Product.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Domain.Entities
 {
-    public class Order : BaseEntity
+    public class Product : BaseEntity
     {
-        public string Description { get; set; }
-        public string Address { get; set; }
-
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public long Price { get; set; }
     }
 }
